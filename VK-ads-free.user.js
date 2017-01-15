76b7dd43cac0579cedd089c7a8b8dd7bb6469304
orlovskyalex <alex94orlovsky@gmail.com>
2017-01-16 00:40:00 +0200

    performance improvements, refactoring, bug fixes

--- VK-ads-free.user.js
+++ VK-ads-free.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              VK-ads-free
 // @description       Removes ads from vk.com/feed
-// @version           2.0.12
+// @version           2.1.0
 // @updateURL         https://openuserjs.org/meta/orlovskyalex/VK-ads-free.meta.js
 // @downloadURL       https://openuserjs.org/src/scripts/orlovskyalex/VK-ads-free.user.js
 // @source            https://github.com/orlovskyalex/VK-ads-free.user.js
@@ -13,148 +13,162 @@
 // @copyright         2016-2017, Alex Orlovsky (https://github.com/orlovskyalex)
 // ==/UserScript==
 
+var $keywords_field = $('<div id="keywords_field" contenteditable="true" autofocus="true">'),
+	$keywords_save = $('<button id="keywords_save_btn" class="flat_button wk_like_wrap _like_wrap">Save</button>'),
+	$keywords_clear = $('<button id="keywords_clear_btn" class="flat_button secondary button_light wl_action_link">Clear</button>'),
+	$script_link = $('<a class="title_link" href="https://github.com/orlovskyalex/VK-ads-free.user.js#vk-ads-free" target="_blank">VK-ads-free</a>'),
+	$script_menu = $('<div id="vk-ads-free_menu" class="popup_box_container">')
+	.append($('<div class="box_layout">')
+		.append($('<div class="box_title_wrap">')
+			.append($('<div class="box_x_button">'))
+			.append($('<div class="box_title">')
+				.append($script_link)
+				.append('2.1.0')))
+		.append($('<div class="box_body box_no_buttons">')
+			.append('<div>Keywords:</div>')
+			.append($keywords_field)
+			.append($('<div class="buttons_wrap clear_fix">')
+				.append($keywords_save)
+				.append($keywords_clear)))),
+	$script_menu_link = $('<a id="top_vk-ads-free_settings_link" class="top_profile_mrow">VK-ads-free</a>'),
+	body,
+	box_layer_bg,
+	box_layer_wrap,
+	$box_layer,
+	keywords;
+
 $(function () {
 
-	var html = $('html'),
-		keysMenu = $('<a id="top_vk-ads-free_settings_link" class="top_profile_mrow">VK-ads-free</a>'),
-		keysForm = $('<div id="keywords_field" contenteditable="true" autofocus="true">'),
-		keysFormLabel = '<div class="label">Keywords:</div>',
-		keysSaveBtn = $('<button id="keywords_save_btn" class="flat_button wk_like_wrap _like_wrap">Save</button>'),
-		keysClearBtn = $('<button id="keywords_clear_btn" class="flat_button secondary button_light wl_action_link">Clear</button>'),
-		keysBlock = $('<div id="keywords_wrapper" class="popup_box_container">')
-		.append($('<div class="box_layout">')
-			.append($('<div class="box_title_wrap">')
-				.append($('<div class="box_x_button">'))
-				.append($('<div class="box_title">')
-					.append($('<a class="title_link" href="https://github.com/orlovskyalex/VK-ads-free.user.js" target="_blank">VK-ads-free</a>'))
-					.append('2.0.12')))
-			.append($('<div class="box_body box_no_buttons">')
-				.append(keysFormLabel)
-				.append(keysForm)
-				.append($('<div class="clear_fix">')
-					.append(keysSaveBtn)
-					.append(keysClearBtn)))),
-		boxlayerbg = $('#box_layer_bg'),
-		boxlayerwrap = $('#box_layer_wrap'),
-		boxlayer = $('#box_layer'),
-		keys,
-		posts;
+	body = document.body;
+	box_layer_bg = document.getElementById('box_layer_bg');
+	box_layer_wrap = document.getElementById('box_layer_wrap');
+	$box_layer = $('#box_layer');
+	keywords;
 
 	// css
-	$('head').append('<link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/orlovskyalex/VK-ads-free.user.js/222fe51ef81655fbf8e6e0c43ab0db0b6183e34f/VK-ads-free.style.css">');
+	$('head').append('<link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/orlovskyalex/VK-ads-free.user.js/e306c7cd9402623f85b67ef857fcf8b12eb189a4/VK-ads-free.style.css">');
 
+	// show guide arrow
 	if (!localStorage.getItem('vk-ads-free_menu_opened') || !localStorage.getItem('vk-ads-free_keywords')) {
-		html.addClass('vk-ads-free_guide');
+		body.classList.add('vk-ads-free_guide');
 	}
 
-	// sets keywords
+	// read keywords from localStorage
 	if (localStorage.getItem('vk-ads-free_keywords')) {
-		keys = localStorage.getItem('vk-ads-free_keywords').split('; ');
+		keywords = localStorage.getItem('vk-ads-free_keywords').split('; ');
 	}
 
-	// removes all visible ads on page
+	// remove all visible ads on page
 	hideAllVisibleAd();
 
-	// removes ads on feed updates
+	// remove ads on feed updates
 	$('#feed_wall').bind('DOMNodeInserted', function (e) {
 		var el = e.target;
-		if ($(el).hasClass('feed_row')) {
+		if (el.classList.contains('feed_row')) {
 			hideAd(el);
 		}
 	});
 
-	keysMenu.insertBefore('#top_logout_link').after('<div class="top_profile_sep">');
+	$script_menu_link.insertBefore('#top_logout_link').after('<div class="top_profile_sep">');
 
-	keysMenu.on('click', keywordsShow);
+	$script_menu_link.on('click', keywordsShow);
 
-	keysBlock.on('click', '.box_x_button', keywordsHide);
-
-	html.on('mouseup', function (e) {
-		if (boxlayer.find('#keywords_wrapper').length && e.target != keysBlock && !keysBlock.has(e.target).length) {
+	$script_menu.on('click', function (e) {
+		if (e.target.classList.contains('box_x_button')) {
 			keywordsHide();
 		}
 	});
 
-	function keywordsShow() {
-		boxlayer.append(keysBlock);
-		boxlayerbg.show();
-		boxlayerwrap.show();
-		if (keys) {
-			var skeys = '';
-			for (var i in keys) {
-				skeys += keys[i];
-				if (i < keys.length - 1) skeys += '; ';
-			}
-			keysForm.text(skeys);
-		}
-		keysForm.focus();
-		keywordsDo();
-	}
+	$script_link.on('click', function () {
+		window.open($(this).attr('href'), '_blank');
+	});
 
-	function keywordsHide() {
-		if (!localStorage.getItem('vk-ads-free_menu_opened')) localStorage.setItem('vk-ads-free_menu_opened', true);
-		localStorage.getItem('vk-ads-free_keywords') ? html.removeClass('vk-ads-free_guide') : html.addClass('vk-ads-free_guide');
-		boxlayerbg.hide();
-		boxlayerwrap.hide();
-		keysBlock.detach();
-	}
-
-	function keywordsDo() {
-		keysSaveBtn.on('click', function () {
-			keys = keysForm.text();
-			if (keys) {
-				localStorage.setItem('vk-ads-free_keywords', keys);
-				keys = keys.split('; ');
-			} else {
-				localStorage.removeItem('vk-ads-free_keywords');
-			}
+	body.addEventListener('mouseup', function (e) {
+		if ($box_layer.find('#vk-ads-free_menu').length && e.target != $script_menu && !$script_menu.has(e.target).length) {
 			keywordsHide();
-			hideAllVisibleAd();
-		});
-
-		keysClearBtn.on('click', function () {
-			keysForm.empty();
-		});
-	}
-
-	function hideAd(el) {
-		var post = $(el),
-			isAd = false,
-			sourceText = post.find('.wall_post_text').text().toLowerCase(),
-			compareText;
-
-		// check if current post is ad
-		if (keys) {
-			for (var i in keys) {
-				compareText = keys[i].toLowerCase();
-				if (sourceText.indexOf(compareText) != -1) {
-					isAd = true;
-				}
-			}
 		}
-		if (post.find('.wall_marked_as_ads').length) isAd = true;
-
-		if (isAd) {
-			var text = post.find('.wall_post_text');
-
-			// creates text content for non-text ads or removes emoji
-			text.length ? text.find('.emoji').remove() : text = '<div class="wall_post_text">' + post.find('.wall_marked_as_ads').text() + '</div>';
-
-			post.find('.post').html('').append(text);
-			post.find('.post').addClass('post_marked_as_ads');
-		}
-
-		// removes advertised applications
-		if (post.find('.ads_ads_news_wrap').length) {
-			post.remove();
-		}
-	}
-
-	function hideAllVisibleAd() {
-		posts = $('.feed_row');
-		posts.each(function () {
-			hideAd(this);
-		});
-	}
+	});
 
 });
+
+function keywordsShow() {
+	$box_layer.append($script_menu);
+	box_layer_bg.style.display = 'block';
+	box_layer_wrap.style.display = 'block';
+	if (keywords) {
+		var skeys = '',
+			i;
+		for (i in keywords) {
+			skeys += keywords[i] + '; ';
+		}
+		$keywords_field.text(skeys.slice(0, -2));
+	}
+	$keywords_field.focus();
+	keywordsDo();
+}
+
+function keywordsHide() {
+	if (!localStorage.getItem('vk-ads-free_menu_opened')) localStorage.setItem('vk-ads-free_menu_opened', true);
+	localStorage.getItem('vk-ads-free_keywords') ? body.classList.remove('vk-ads-free_guide') : body.classList.add('vk-ads-free_guide');
+	box_layer_bg.style.display = 'none';
+	box_layer_wrap.style.display = 'none';
+	$script_menu.detach();
+}
+
+function keywordsDo() {
+	$keywords_save.on('click', function () {
+		keywords = $keywords_field.text();
+		if (keywords) {
+			localStorage.setItem('vk-ads-free_keywords', keywords);
+			keywords = keywords.split('; ');
+		} else {
+			localStorage.removeItem('vk-ads-free_keywords');
+		}
+		keywordsHide();
+		hideAllVisibleAd();
+	});
+
+	$keywords_clear.on('click', function () {
+		$keywords_field.empty();
+	});
+}
+
+function hideAd(el) {
+	var post = $(el),
+		isAd = false;
+
+	// check if current post is ad
+	if (keywords) {
+		var sourceText = post.find('.wall_post_text').text().toLowerCase(),
+			compareText;
+		for (var i in keywords) {
+			compareText = keywords[i].toLowerCase();
+			if (sourceText.indexOf(compareText) != -1) {
+				isAd = true;
+			}
+		}
+	}
+	if (post.find('.wall_marked_as_ads').length) isAd = true;
+
+	if (isAd) {
+		var text = post.find('.wall_post_text');
+
+		// create text content for non-text ads or removes emoji
+		text.length ? text.find('.emoji').remove() : text = '<div class="wall_post_text">' + post.find('.wall_marked_as_ads').text() + '</div>';
+
+		// replace original content with preview
+		post.find('.post').addClass('post_marked_as_ads').empty().append(text);
+	}
+
+	// remove advertised applications
+	if (post.find('.ads_ads_news_wrap').length) {
+		post.remove();
+	}
+}
+
+function hideAllVisibleAd() {
+	var $posts = $('.feed_row');
+	$posts.each(function () {
+		hideAd(this);
+	});
+}
